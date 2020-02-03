57b5dff2de82fe757da08a1777448c5fe75055ad
Sam Zaydel <szaydel@gmail.com>
2020-02-02 19:47:37 -0800

    Adding more String methods tests

--- tests/test_ex1.py
+++ tests/test_ex1.py
@@ -6,6 +6,7 @@ from integraty.case import run_integra_tests
 from integraty.extprog import ExternalProgram, ExternalProgramException
 from integraty.productivity import TemporaryFile, ChecksumStream
 
+
 class LibraryUsageEx1(IntegraTestCase):
 
     @classmethod
@@ -25,8 +26,8 @@ class LibraryUsageEx1(IntegraTestCase):
               file=sys.stderr)
 
     def test_domain_name_is_almost_equal(self):
-        cmd = 'dig -t TXT microsoft.com'
         self.log.info("Tests string near equality assertions")
+        cmd = 'dig -t TXT microsoft.com'
         with ExternalProgram(cmd) as c:
             c.exec()
             self.assertCommandSucceeded(c)
@@ -43,6 +44,7 @@ class LibraryUsageEx1(IntegraTestCase):
                                                     ratio=0.5)
 
     def test_to_dict_mapping(self):
+        self.log.info("Tests mapping function to dict translation")
 
         def func(line: str) -> zip:
             if len(line.split()) < 2:
@@ -51,7 +53,6 @@ class LibraryUsageEx1(IntegraTestCase):
             return zip([tokens[4]], [tokens[0]])
 
         cmd = 'dig -x -t NS microsoft.com'
-        self.log.info("Tests mapping function to dict translation")
         with ExternalProgram(cmd) as c:
             c.exec()
             self.assertCommandSucceeded(c)
@@ -71,8 +72,8 @@ class LibraryUsageEx1(IntegraTestCase):
             self.assertSetEqual(expected_keys, actual_keys)
 
     def test_with_prefix(self):
-        cmd = self.get_class_var('whois_cloudflare_com_cmd')
         self.log.info("Tests expected behaviour of with_prefix method")
+        cmd = self.get_class_var('whois_cloudflare_com_cmd')
         with ExternalProgram(cmd) as c:
             c.exec()
             self.assertCommandSucceeded(c)
@@ -88,18 +89,73 @@ class LibraryUsageEx1(IntegraTestCase):
             self.assertNotIn('ns9.cloudflare.com', results)
 
     def test_with_suffix(self):
+        self.log.info("Tests expected behaviour of the with_suffix method")
         cmd = self.get_class_var('whois_cloudflare_com_cmd')
-        self.log.info("Tests expected behaviour of with_suffix method")
         with ExternalProgram(cmd) as c:
             c.exec()
             self.assertCommandSucceeded(c)
-            results = [ns.split(':')[0] for ns in c.out.with_suffix('REDACTED')]
+            results = [
+                ns.split(':')[0] for ns in c.out.with_suffix('REDACTED')
+            ]
             # This is something of a smoke test example.
             self.assertGreaterEqual(len(results), 48)
             self.assertIn('Registrant Name', results)
             self.assertIn('Tech Country', results)
             self.assertIn('Tech Name', results)
             self.assertNotIn('Registrar', results)
+
+    def test_fields(self):
+        self.log.info("Tests expected behaviour of the fields method")
+        cmd = self.get_class_var('whois_cloudflare_com_cmd')
+        with ExternalProgram(cmd) as c:
+            c.exec()
+            self.assertCommandSucceeded(c)
+            domain_name, = c.out.fields(pattern='Domain Name')[2]
+            registrar_url, = c.out.fields(pattern='Registrar URL')[2]
+            self.assertEqual(domain_name.lower(), 'cloudflare.com')
+            self.assertEqual(registrar_url, 'https://www.cloudflare.com')
+
+        cmd = 'dig -t AAAA cloudflare.com'
+        with ExternalProgram(cmd) as c:
+            c.exec()
+            self.assertCommandSucceeded(c)
+            results = c.out.fields(pattern='[;]', exclude=True)
+            self.assertDictEqual(
+                dict(zip(results[4], results[0])), {
+                    '2606:4700::6811:af55': 'cloudflare.com.',
+                    '2606:4700::6811:b055': 'cloudflare.com.'
+                })
+
+    def test_take_column(self):
+        cmd = 'host -t AAAA cloudflare.com'
+        self.log.info("Tests expected behaviour of the take_column method")
+        with ExternalProgram(cmd) as c:
+            c.exec()
+            self.assertCommandSucceeded(c)
+            results = c.out.take_column(column=4)
+            # This is arguably fragile as well, because over time these may
+            # change, or new quad records may get added.
+            self.assertSetEqual(
+                set(results),
+                set(['2606:4700::6811:af55', '2606:4700::6811:b055']))
+            # A better test may be to see if we are finding quad records, or
+            # if we are certain at least these two particular addresses must
+            # exist, we can assert their presence in the sequence like so.
+            self.assertIn('2606:4700::6811:af55', results)
+            self.assertIn('2606:4700::6811:b055', results)
+
+        cmd = 'host -t MX cloudflare.com'
+        with ExternalProgram(cmd) as c:
+            c.exec()
+            self.assertCommandSucceeded(c)
+            results = c.out.take_column(column=6)
+            self.assertSetEqual(
+                set(results),
+                set([
+                    'aspmx.l.google.com.', 'alt1.aspmx.l.google.com.',
+                    'alt2.aspmx.l.google.com.', 'aspmx2.googlemail.com.',
+                    'aspmx3.googlemail.com.'
+                ]))
 
 
 if __name__ == "__main__":
